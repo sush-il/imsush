2b52d82244e4efa9e7c6a268ad635a7a42ba01ad
Sushil Bhandari <zoroat3@gmail.com>
2024-07-22 10:31:28 +0100

    Added a admin page Added login page to get acess to admin

---
+++ src/pages/adminPage.tsx
@@ -0,0 +1,157 @@
+import { useState, useEffect } from 'react';
+import AddButton from '../components/adminAddButton';
+import SidePanel from '../components/adminSidePanel';
+import { buildsDataType, projectDataType } from '../utils/dataTypes';
+import Login from './login';
+
+const AdminPage = () => {
+
+  const isAuthenticated = sessionStorage.getItem("isAuthenticated");
+
+  if(isAuthenticated !== "true"){
+    return <Login />
+  }
+
+  const pantryId = import.meta.env.VITE_PANTRYID;
+  const basketName = import.meta.env.VITE_PANTRYBASKETNAME;
+
+  const [isSidePanelOpen, setSidePanelOpen] = useState(false);
+  const [projects, setProjects] = useState<projectDataType[]>([]);
+  const [builds, setBuilds] = useState<buildsDataType>({'web':[],  'leetcode':[], 'other': [] });
+  
+  useEffect(() => {
+    const allCurrentProjects = sessionStorage.getItem("currentProjects");
+    const allCurrentBuilds = sessionStorage.getItem("currentBuildsData");
+    
+    if (allCurrentProjects !== null) {
+      try {
+        const parsedProjects = JSON.parse(allCurrentProjects) as projectDataType[];
+        const parsedBuilds = JSON.parse(allCurrentBuilds) as buildsDataType;
+        setProjects(parsedProjects);
+        setBuilds(parsedBuilds);
+      } catch (error) {
+        console.error('Error parsing projects data:', error);
+      }
+    } else {
+      console.log('No projects found in sessionStorage');
+    }
+  }, []);
+
+
+  const handleAddButtonClick = () => {
+    setSidePanelOpen(true);
+  };
+
+  const addItem = async(type:string, newData: projectDataType[] | buildsDataType) => {
+    let dataToAdd = null;
+
+    if(type === 'project'){dataToAdd = {projects: newData}}
+    else if (type === 'build'){dataToAdd = {buildData: newData }}
+
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dataToAdd)
+    }
+    try{
+      const response = await fetch(`https://getpantry.cloud/apiv1/pantry/${pantryId}/basket/${basketName}`, options)
+    }catch(error){
+      console.error("Couldn't add item to the API: " + error)
+    }
+  }
+
+  const closeSidePanel = (type?:"project" | "build" | "card" | null, newData?: projectDataType | buildsDataType) => {
+    if(type === 'project' && newData){
+      const dataToAdd = [...projects, newData];
+      addItem(type, [newData]);
+      setProjects(dataToAdd);
+    } 
+    else if(type === 'build' && newData){
+        if(newData.web.length > 0){
+          builds.web.push(newData.web[0]);
+          addItem(type, builds);
+        }
+        else if(newData.leetcode.length > 0){
+          builds.leetcode.push(newData.leetcode[0]);
+          addItem(type, builds);
+        }
+        else if(newData.other.length > 0){
+          builds.other.push(newData.other[0]);
+          addItem(type, builds);
+        }
+    }
+    
+    setSidePanelOpen(false);
+
+  };
+
+  return (
+    <div className="w-full min-h-screen font-mono flex flex-col bg-slate-200 text-black dark:bg-gray-900  dark:text-white p-3">
+      <h1 className='p-3'>Welcome to the Admin Page</h1>
+      <h1 className='text-4xl font-bold p-3'> Projects </h1>
+      <div className='grid grid-cols-4 gap-3'>
+        {projects.map((project, index) => (
+          <div key={index} className="mb-4 p-4 shadow rounded">
+            <h2 className="text-2xl font-bold">{project.title}</h2>
+            <p>{project.description}</p>
+            <a href={project.githubLink} className="text-blue-600">{project.githubLink}</a>
+            {project.featured === "true" && (
+              <div className="mt-2 text-green-600 font-bold">Featured</div>
+            )}
+            <div className="mt-2">
+              <strong>Tech Used:</strong> {project.techUsed.join(', ')}
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <h1 className='text-4xl font-bold p-3'> Builds </h1>
+      <div className='grid grid-cols-4 gap-3'>
+        {builds.web.map((build, index) => {
+          let preType;
+          if(build.type === "video"){
+            preType = `https://www.youtube.com/watch?v=${build.id}`
+          }else if(build.type === "playlist"){
+            preType = `https://www.youtube.com/playlist?list=${build.id}`
+          }
+          return <div key={index} className="mb-4 p-4 shadow rounded">
+            <h2 className="text-2xl font-bold">{build.type}</h2>
+            <a target='_blank' href={preType}> {build.id}</a>
+          </div>
+        })}
+        
+        {builds.other.map((build, index) => {
+          let preType;
+          if(build.type === "video"){
+            preType = `https://www.youtube.com/watch?v=${build.id}`
+          }else if(build.type === "playlist"){
+            preType = `https://www.youtube.com/playlist?list=${build.id}`
+          }
+          return <div key={index} className="mb-4 p-4 shadow rounded">
+            <h2 className="text-2xl font-bold">{build.type}</h2>
+            <a target='_blank' href={preType}> {build.id}</a>
+          </div>
+        })}
+        
+        {builds.leetcode.map((build, index) => {
+          let preType;
+          if(build.type === "video"){
+            preType = `https://www.youtube.com/watch?v=${build.id}`
+          }else if(build.type === "playlist"){
+            preType = `https://www.youtube.com/playlist?list=${build.id}`
+          }
+          return <div key={index} className="mb-4 p-4 shadow rounded">
+            <h2 className="text-2xl font-bold">{build.type}</h2>
+            <a target='_blank' href={preType}> {build.id}</a>
+          </div>
+        })}
+      </div>
+      <AddButton onClick={handleAddButtonClick} />
+      {isSidePanelOpen && <SidePanel onClose={closeSidePanel} />}
+    </div>
+  );
+};
+
+export default AdminPage;
---
+++ src/pages/login.tsx
@@ -0,0 +1,84 @@
+import {useState } from "react"
+import { authorizeAdmin } from "../utils/appFunctions"
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [enteredUsername,setEnteredUsername] = useState("")
+  const [enteredPassword,setEnteredPassword] = useState("")
+
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8  dark:bg-gray-900">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 dark:text-white">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={e => e.preventDefault()} className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required autoFocus
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={e => setEnteredUsername(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 dark:text-white">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required autoFocus
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={e => setEnteredPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                onClick={async () => {
+                  const isAuthenticated = await authorizeAdmin(enteredUsername, enteredPassword);
+                  if(isAuthenticated){
+                    sessionStorage.setItem("isAuthenticated", "true");
+                    navigate(0);
+                  }
+                }}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
